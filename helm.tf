504426fc3bad661b3e9c86c9acb9d52df39f522b
Apoorv Dubey <apoorv.dubey@paytm.com>
2025-02-12 23:54:09 +0530

    helm updated agin

--- helm.tf
+++ helm.tf
@@ -29,7 +29,8 @@ resource "helm_release" "deepseek_gpu" {
         memory: 30G
         nvidia.com/gpu: "1"
 
-    command: "vllm serve deepseek-ai/DeepSeek-R1-Distill-Llama-8B --max_model 2048"
+    command: |
+      vllm serve deepseek-ai/DeepSeek-R1-Distill-Llama-8B --max_model 2048
     EOT
   ]
   depends_on = [module.eks, kubernetes_manifest.gpu_nodepool]
