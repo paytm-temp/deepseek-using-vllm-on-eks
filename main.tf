befdab06c7795e8ca56492fab3f04b596d5a5f28
Tushar Aggarwal <tushar1.aggarwal@paytm.com>
2025-02-14 00:02:28 +0530

    Added nodegroup

--- main.tf
+++ main.tf
@@ -1,24 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0"
-    }
-    time = {
-      source  = "hashicorp/time"
-      version = ">= 0.9.1"
-    }
-    http = {
-      source  = "hashicorp/http"
-      version = ">= 3.0"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 3.0"
-    }
-  }
-}
-
 variable "enable_deep_seek_gpu" {
   description = "Enable DeepSeek using GPUs"
   type        = bool
@@ -48,41 +27,34 @@ locals {
   }
 }
 
-
 # Define the required providers
 provider "aws" {
   region = local.region # Change to your desired region
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
     args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
   }
-  ignore_annotations = [
-    "^karpenter.sh/.*"
-  ]
 }
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
+      # This requires the awscli to be installed locally where Terraform is executed
       args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
     }
-  }
-  experiments {
-    manifest = true
   }
 }
 
@@ -134,59 +106,27 @@ module "eks" {
 
   cluster_compute_config = {
     enabled    = true
-    node_pools = ["general-purpose"]  # Add back for initial cluster setup
+    node_pools = ["general-purpose"]
   }
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  cluster_addons = {
-    vpc-cni = {
-      most_recent = true
-    }
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
+  node_groups = {
+    eks_nodes = {
+      desired_capacity = 2  # Reduced to 2 nodes
+      max_capacity     = 2  # Max capacity set to 2
+      min_capacity     = 2  # Min capacity set to 2
+
+      instance_type = "g4dn.2xlarge"  # Using g4dn.2xlarge (32GB RAM, 8 vCPUs, GPU support)
+      
+      subnet_ids = module.vpc.private_subnets
+
+      tags = local.tags
     }
   }
 
   tags = local.tags
-}
-
-# Add this after time_sleep
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
-
-  depends_on = [
-    module.eks,
-    time_sleep.wait_for_kubernetes
-  ]
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
-
-  depends_on = [
-    module.eks,
-    time_sleep.wait_for_kubernetes
-  ]
-}
-
-data "http" "wait_for_cluster" {
-  url = module.eks.cluster_endpoint
-
-  depends_on = [
-    data.aws_eks_cluster.cluster
-  ]
-}
-
-# Add this after the eks module
-resource "time_sleep" "wait_for_kubernetes" {
-  depends_on = [module.eks]
-
-  create_duration = "20s"
 }
 
 resource "aws_ecr_repository" "chatbot-ecr" {
@@ -197,120 +137,6 @@ resource "aws_ecr_repository" "chatbot-ecr" {
 resource "aws_ecr_repository" "neuron-ecr" {
   name                 = "${local.name}-neuron-base"
   image_tag_mutability = "MUTABLE"
-}
-
-# Add after the eks module but before the nodepool resources
-module "karpenter" {
-  source  = "terraform-aws-modules/eks/aws//modules/karpenter"
-  version = "20.33.1"
-
-  cluster_name = module.eks.cluster_name
-
-  irsa_oidc_provider_arn          = module.eks.oidc_provider_arn
-  irsa_namespace_service_accounts = ["karpenter:karpenter"]
-}
-
-# Create the IAM role for Karpenter
-resource "aws_iam_role_policy_attachment" "karpenter_policy" {
-  role       = module.karpenter.iam_role_name
-  policy_arn = aws_iam_policy.karpenter.arn
-}
-
-resource "aws_iam_policy" "karpenter" {
-  name = "KarpenterControllerPolicy-${local.name}"
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "ec2:CreateLaunchTemplate",
-          "ec2:CreateFleet",
-          "ec2:RunInstances",
-          "ec2:CreateTags",
-          "iam:PassRole",
-          "ec2:TerminateInstances",
-          "ec2:DescribeLaunchTemplates",
-          "ec2:DescribeInstances",
-          "ec2:DescribeSecurityGroups",
-          "ec2:DescribeSubnets",
-          "ec2:DescribeInstanceTypes",
-          "ec2:DescribeInstanceTypeOfferings",
-          "ec2:DescribeAvailabilityZones",
-          "ssm:GetParameter"
-        ]
-        Resource = ["*"]
-      }
-    ]
-  })
-}
-
-resource "helm_release" "karpenter" {
-  namespace        = "karpenter"
-  create_namespace = true
-
-  name       = "karpenter"
-  repository = "oci://public.ecr.aws/karpenter"
-  chart      = "karpenter"
-  version    = "v0.33.1"
-
-  set {
-    name  = "settings.aws.clusterName"
-    value = module.eks.cluster_name
-  }
-
-  set {
-    name  = "settings.aws.clusterEndpoint"
-    value = module.eks.cluster_endpoint
-  }
-
-  set {
-    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = module.karpenter.iam_role_arn
-  }
-
-  depends_on = [
-    module.eks,
-    module.karpenter,
-    time_sleep.wait_for_kubernetes
-  ]
-}
-
-# Add this nodeclass manifest
-resource "kubernetes_manifest" "default_nodeclass" {
-  manifest = {
-    apiVersion = "karpenter.k8s.aws/v1beta1"
-    kind       = "NodeClass"
-    metadata = {
-      name = "default"
-    }
-    spec = {
-      amiFamily = "AL2"
-      role      = module.karpenter.iam_role_name
-      subnetSelectorTerms = [
-        {
-          tags = {
-            "kubernetes.io/role/internal-elb" = "1"
-          }
-        }
-      ]
-      securityGroupSelectorTerms = [
-        {
-          tags = {
-            "kubernetes.io/cluster/${module.eks.cluster_name}" = "owned"
-          }
-        }
-      ]
-    }
-  }
-
-  depends_on = [
-    helm_release.karpenter,
-    data.http.wait_for_cluster,
-    data.aws_eks_cluster.cluster,
-    data.aws_eks_cluster_auth.cluster,
-    null_resource.wait_for_cluster
-  ]
 }
 
 # Outputs
@@ -326,13 +152,3 @@ output "ecr_repository_uri" {
 output "ecr_repository_uri_neuron" {
   value = aws_ecr_repository.neuron-ecr.repository_url
 }
-
-# Add this null resource to enforce ordering
-resource "null_resource" "wait_for_cluster" {
-  depends_on = [
-    module.eks,
-    time_sleep.wait_for_kubernetes,
-    data.http.wait_for_cluster
-  ]
-}
-
