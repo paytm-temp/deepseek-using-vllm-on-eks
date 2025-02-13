8cea7ac0382b5ad9286c9f130a3d311d9698cf37
Tushar Aggarwal <tushar1.aggarwal@paytm.com>
2025-02-13 23:19:43 +0530

    added auth

--- main.tf
+++ main.tf
@@ -33,9 +33,15 @@ provider "aws" {
   region = local.region # Change to your desired region
 }
 
+# Add this data source to get EKS cluster auth
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
+
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
