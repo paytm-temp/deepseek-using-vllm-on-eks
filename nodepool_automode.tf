a4b9bb5a90e8be22814404db6990ddaeb76e7459
Apoorv Dubey <apoorv.dubey@paytm.com>
2025-02-16 17:11:43 +0530

    added disruption policy

--- nodepool_automode.tf
+++ nodepool_automode.tf
@@ -7,6 +7,10 @@ resource "kubernetes_manifest" "gpu_nodepool" {
       name = "gpu-nodepool"
     }
     spec = {
+      disruption = {
+        consolidationPolicy = "WhenUnderutilized"
+        expireAfter = "168h"  # 7 days
+      }
       limits = {
         "cpu" = "8"     # Full g4dn.2xlarge CPU
         "memory" = "32Gi"  # One g4dn.2xlarge memory
